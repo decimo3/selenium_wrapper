ea5d230c6dde4603747cf144ed3edd9422a379f5
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2026-08-20 15:12:18 -0300

    fix: implement dynamic package versioning based on git tag version

--- release.sh
+++ release.sh
@@ -10,18 +10,6 @@ fi
 # DONE - Fetch tags and get the latest one
 git fetch --tags
 version=$(git describe --tags $(git rev-list --tags --max-count=1))
-version_number="${version#v}"  # Remove 'v' prefix if the tag has it
-echo "Version: $version_number"
-
-# Extract major, minor, and patch versions
-IFS='.' read -r MAJOR_VERSION MINOR_VERSION PATCH_VERSION <<< "$version_number"
-export MAJOR_VERSION MINOR_VERSION PATCH_VERSION
-echo "Major version: $MAJOR_VERSION"
-echo "Minor version: $MINOR_VERSION"
-echo "Patch version: $PATCH_VERSION"
-
-# Write package descriptor file
-sed -i "s/0.0.0/${MAJOR_VERSION}.${MINOR_VERSION}.${PATCH_VERSION}/g" pyproject.toml
 
 # Install dependencies
 pip install -e ".[test]"
@@ -40,6 +28,3 @@ gh release create $version --verify-tag \
     --title "${version} ($(date +%Y-%m-%d))" \
     --notes-file release_notes.md \
     "dist/*"
-
-# Reverting placeholder files
-git restore pyproject.toml
